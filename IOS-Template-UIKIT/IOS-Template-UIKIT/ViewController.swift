0d2fb5d06eef4c8a198f9c02c264bcefd771b7f7
Abhishek Babladi <abhishek.babladi@netlight.com>
2023-02-22 14:42:16 +0100

    "first commit"
    git commit -m "first commit"

---
+++ IOS-Template-UIKIT/IOS-Template-UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Template-UIKIT
+//
+//  Created by Abhishek Babladi on 2023-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
